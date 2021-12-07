02cb6411d624b432f96eb5d769e4282c4fd7b9bb
isaksengeir <geir.isaksen@uit.no>
2021-12-07 18:48:58 +0100

    Day 7 solutions - cleaning up a bit... should do more though

--- 07/day7.py
+++ 07/day7.py
@@ -2,28 +2,27 @@
 import numpy as np
 
 
-def part1(crab_pos):
+def part1_fuel(c, pos):
+    return sum([abs(x - c) for x in pos])
+
+
+def part2_fuel(c, pos):
+    return sum([(abs(x - c) * (abs(x - c) + 1)) / 2 for x in pos])
+
+
+def calc_fuel(part1=True):
     c = int(np.average(crab_pos) / 2)
-    fuel_min = sum([abs(x - c) for x in crab_pos])
+    if part1:
+        fuel_min = part1_fuel(c, crab_pos)
+    else:
+        fuel_min = part2_fuel(c, crab_pos)
     fuel_increased = False
     while not fuel_increased:
         c += 1
-        fuel_c = sum([abs(x - c) for x in crab_pos])
-        if fuel_c <= fuel_min:
-            fuel_min = fuel_c
+        if part1:
+            fuel_c = part1_fuel(c, crab_pos)
         else:
-            c -= 1
-            fuel_increased = True
-    return fuel_min
-
-
-def part2(crab_pos):
-    c = int(np.average(crab_pos) / 2)
-    fuel_min = sum([(abs(x - c)*(abs(x - c)+1))/2 for x in crab_pos])
-    fuel_increased = False
-    while not fuel_increased:
-        c += 1
-        fuel_c = sum([(abs(x - c)*(abs(x - c)+1))/2 for x in crab_pos])
+            fuel_c = part2_fuel(c, crab_pos)
         if fuel_c <= fuel_min:
             fuel_min = fuel_c
         else:
@@ -34,6 +33,5 @@ def part2(crab_pos):
 
 if __name__ == "__main__":
     crab_pos = np.loadtxt("data.txt", delimiter=",", dtype=int)
-
-    print(part1(crab_pos))
-    print(part2(crab_pos))
+    print(f"PART 1: {calc_fuel()} ")
+    print(f"PART 2: {calc_fuel(part1=False)} ")
